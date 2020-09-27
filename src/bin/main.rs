ac6bae24dc29281ff798775107506f95209beb69
papageorge <4denosinetriphosphate@gmail.com>
2020-09-27 10:13:01 -0400

    full parallel works - writing tests and docstrings

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,8 @@
 use ndarray_csv::{Array2Reader, Array2Writer};
-use std::{cell::RefCell, collections::HashMap, thread::sleep, time::Duration, thread, sync::RwLock, sync::Arc};
+use ndarray::array;
+use std::{cell::RefCell, collections::HashMap, thread::sleep, time::Duration, thread, sync::RwLock, sync::Arc, sync::mpsc};
 
+/// Split up the board into non-overlapping sub-boards.
 static NUM_ROW_GROUPS: u64 = 3;
 static NUM_COL_GROUPS: u64 = 3;
 
@@ -11,6 +13,22 @@ Any live cell with more than three live neighbours dies, as if by overpopulation
 Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 */
 
+/// Flip every value in a board.
+///
+/// # Arguments
+/// * `brd` - the board.
+/// * `rows` - the number of rows.
+/// * `cols` - the number of columns.
+///
+/// ```
+/// use ndarray::array;
+///
+/// let mut arr = array![[0, 1], [1, 0]];
+/// let r = arr.shape()[0];
+/// let c = arr.shape()[1];
+/// flip_board(&mut arr, &r, &c);
+/// assert_eq!(arr, array![[1, 0], [0, 1]]);
+/// ```
 fn flip_board(brd: &mut ndarray::Array2<u8>, rows: &usize, cols: &usize) {
     for r in 0..*rows {
         for c in 0..*cols {
@@ -23,12 +41,60 @@ fn flip_board(brd: &mut ndarray::Array2<u8>, rows: &usize, cols: &usize) {
     }
 }
 
+/// Add all the values in a board at a given collection of indices.
+///
+/// # Arguments
+/// `brd` - the board.
+/// `pos_arr` - the array of positions to sum.
+/// # Returns
+/// The sum.
+///
+/// ```
+/// use ndarray::array;
+///
+/// let mut arr = array![[1, 2], [3, 4]];
+/// assert_eq!(gather_board_values(&arr, &[(0, 0), (1, 1)]), 5);
+/// ```
 fn gather_board_values(brd: &ndarray::Array2<u8>, pos_arr: &[(usize, usize)]) -> u8 {
     let mut ret: u8 = 0;
     for (r0, c0) in pos_arr.iter() { ret += brd[[*r0, *c0]]; }
     return ret
 }
 
+/// Determine the sum of all the neighbors of a given cell.
+///
+/// # Arguments
+/// `brd` - the board.
+/// `rows` - the number of rows.
+/// `cols` - the number of columns.
+/// `r` - the row of the cell.
+/// `c` - the column of the cell.
+///
+/// # Returns
+/// The sum of all neighbors of a particular cell.
+///
+/// ```
+/// use ndarray::array;
+///
+/// let mut arr = array![[0, 1, 2],
+///                      [3, 4, 5],
+///                      [6, 7, 8]];
+///
+/// let r = arr.shape()[0];
+/// let c = arr.shape()[1];
+///
+/// assert_eq!(count_neighbors(&arr, &r, &c, &0, &0), 8);
+/// assert_eq!(count_neighbors(&arr, &r, &c, &0, &1), 14);
+/// assert_eq!(count_neighbors(&arr, &r, &c, &0, &2), 10);
+///
+/// assert_eq!(count_neighbors(&arr, &r, &c, &1, &0), 18);
+/// assert_eq!(count_neighbors(&arr, &r, &c, &1, &1), 32);
+/// assert_eq!(count_neighbors(&arr, &r, &c, &1, &2), 22);
+///
+/// assert_eq!(count_neighbors(&arr, &r, &c, &2, &0), 14);
+/// assert_eq!(count_neighbors(&arr, &r, &c, &2, &1), 26);
+/// assert_eq!(count_neighbors(&arr, &r, &c, &2, &2), 16);
+/// ```
 fn count_neighbors(brd: &ndarray::Array2<u8>,
                    rows: &usize,
                    cols: &usize,
@@ -152,8 +218,8 @@ fn main() {
                                               .from_path(csv_path)
                                               .expect("Cannot read file");
 
-    let mut data0: ndarray::Array2<u8> = reader.deserialize_array2_dynamic().unwrap();
-    let mut data1: RwLock<ndarray::Array2<u8>> = RwLock::new(data0.to_owned());
+    let data0: ndarray::Array2<u8> = reader.deserialize_array2_dynamic().unwrap();
+    let data1: Arc<RwLock<ndarray::Array2<u8>>> = Arc::new(RwLock::new(data0.to_owned()));
 
     let rows: usize = data0.shape()[0];
     let cols: usize = data0.shape()[1];
@@ -186,52 +252,52 @@ fn main() {
     }
 
     // let life_board_rwlock = boards.get_mut(&1).unwrap();
-    // let mut handles: Vec<thread::JoinHandle<_>> = vec![];
-    let mut moves: Vec<(usize, usize, u8)> = vec![];
+    let mut handles: Vec<thread::JoinHandle<_>> = vec![];
+
+    // let mut tx_vec: Vec<Arc<mpsc::Sender<(usize, usize, u8)>>> = vec![];
+    // for _ in 1..extents.len() {
+    //     let new_tx = (&tx).clone();
+    //     tx_vec.push(Arc::new(new_tx));
+    // }
+    // tx_vec.push(Arc::new(tx));
+
     for iter in 0..50 {
+
+        let (tx, rx): (mpsc::Sender<(usize, usize, u8)>,
+               mpsc::Receiver<(usize, usize, u8)>) = mpsc::channel();
 
         // Clear screen.
         print!("{}[2J", 27 as char);
         sleep(Duration::from_millis(500 as u64));
         { print_board(&(data1.read().unwrap()), &rows, &cols, &iter); }
+
         for &(r0, rl, c0, cl) in &extents {
-            {
-                for mv in capture_moves(&(data1.read().unwrap()),
-                                        &rows,
-                                        &cols,
-                                        &r0,
-                                        &rl,
-                                        &c0,
-                                        &cl) {
-                    moves.push(mv);
-                }
-            }
-
-            // handles.push(
-            //     thread::spawn(move || {
-            //         // println!("{} {} {} {}", r0, rl, c0, cl);
-            //         capture_moves(&data1,
-            //                             &rows,
-            //                             &cols,
-            //                             &r0,
-            //                             &rl,
-            //                             &c0,
-            //                             &cl);
-            //     }
-            //     )
-            // );
+            let par_data = data1.clone();
+            let par_tx = (&tx).clone();
+            handles.push(
+                thread::spawn(move || {
+                    for mv in capture_moves(&(par_data.read().unwrap()),
+                                            &rows,
+                                            &cols,
+                                            &r0,
+                                            &rl,
+                                            &c0,
+                                            &cl) {
+                        par_tx.send(mv).unwrap();
+                    }
+                })
+            );
         }
-        // while handles.len() > 0 {
-        //     let h = handles.pop().unwrap();
-        //     h.join().unwrap();
-        // }
+        while handles.len() > 0 {
+            let h = handles.pop().unwrap();
+            h.join().unwrap();
+        }
 
-        while moves.len() > 0 {
-            let (r, c, v) = moves.pop().unwrap();
-            {
-                // let mut life_board = life_board_rwlock.clone().write().unwrap();
-                data1.write().unwrap()[[r, c]] = v;
-            }
+        drop(tx);
+
+        for mv in &rx {
+            let (r, c, v) = mv;
+            { data1.write().unwrap()[[r, c]] = v; }
         }
     }
 
